e272f88ba5b440610012604eb02256916cac9571
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-02-24 18:05:32 -0500

    Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+
+        String name;
+        String Alice = "Alice";
+        String alice = "alice";
+        String Bob = "Bob";
+        String bob = "bob";
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Who are you? What's ya name? ");
+        name = in.nextLine();
+
+        if (name.equals(Alice)){
+            System.out.println("Hi Alice.");
+        } else if (name.equals(alice)){
+            System.out.println("Hi Alice. Please capitalize your name");
+        } else if (name.equals(Bob)){
+            System.out.println("Hi Bob.");
+        } else if (name.equals(bob)){
+            System.out.println(("Hi Bob. Please capitalize your name."));
+        } else {
+            System.out.println(("Who you?"));
+        }
+
 
     }
 }
